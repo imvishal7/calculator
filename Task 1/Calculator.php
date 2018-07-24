289ea05ac54c53db18c3c696f344b9f68d74da70
imvishal7 <vishal.bhoi118@gmail.com>
2018-07-25 00:23:07 +0530

    Formatted

--- Task 1/Calculator.php
+++ Task 1/Calculator.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
  * Description : Command Line Calculator - Sum only for zero to two numbers
  * @author Vishal Bhoi <vishal.bhoi118@gmail.com>
@@ -8,22 +9,22 @@ include("../CommonClass.php");
 
 //Exception handling
 try {
-	array_shift($argv);
-	$strFuncName = array_shift($argv); // get action name
-	$strParams = array_shift($argv); // get parameters
+    array_shift($argv);
+    $strFuncName = array_shift($argv); // get action name
+    $strParams = array_shift($argv); // get parameters
 
-	$objCommonClass = new CommonClass();
-	if(method_exists($objCommonClass, $strFuncName) && $strFuncName == "sum") { // to check method exists or not
-		$arrParams = explode(",",$strParams);
-		$intTotalParams = count($arrParams);
-		if($intTotalParams <= 2){ // check only for zero to two numbers
-			echo $objCommonClass->sum($strParams,1);
-		}else{
-			echo "Enter only zero to two numbers";
-		}
-	} else {
-		throw new Exception("Invalid method..!", 1);
-	}
+    $objCommonClass = new CommonClass();
+    if (method_exists($objCommonClass, $strFuncName) && $strFuncName == "sum") { // to check method exists or not
+        $arrParams = explode(",", $strParams);
+        $intTotalParams = count($arrParams);
+        if ($intTotalParams <= 2) { // check only for zero to two numbers
+            echo $objCommonClass->sum($strParams, 1);
+        } else {
+            echo "Enter only zero to two numbers";
+        }
+    } else {
+        throw new Exception("Invalid method..!", 1);
+    }
 } catch (Exception $e) {
-    echo 'Caught exception: ',  $e->getMessage(), "\n";
+    echo 'Caught exception: ', $e->getMessage(), "\n";
 }
